6b91807e0862eabc605ddf62c0afa945ce46e6ab
Dalton Saffe <daltonsaffe@gmail.com>
2019-02-20 16:01:31 -0500

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,6 @@ import logo from '../img/logo.svg';
 import facebook from '../img/social/facebook.svg';
 import instagram from '../img/social/instagram.svg';
 import twitter from '../img/social/twitter.svg';
-import vimeo from '../img/social/vimeo.svg';
 
 const Footer = class extends React.Component {
   render() {
@@ -44,16 +43,6 @@ const Footer = class extends React.Component {
                         Form Examples
                       </Link>
                     </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
                   </ul>
                 </section>
               </div>
@@ -62,7 +51,7 @@ const Footer = class extends React.Component {
                   <ul className="menu-list">
                     <li>
                       <Link className="navbar-item" to="/blog">
-                        Latest Stories
+                        Archive
                       </Link>
                     </li>
                     <li>
@@ -96,13 +85,6 @@ const Footer = class extends React.Component {
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
