909485f659a023305cce4b04662146cf48cdc99f
Dalton Saffe <daltonsaffe@gmail.com>
2019-03-18 00:56:07 -0400

    delete index query

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import BlogRoll from '../components/BlogRoll';
 
-export const IndexPageTemplate = ({ image }) => (
+export const IndexPageTemplate = () => (
   <div>
     <section className="section">
       <div className="container">
@@ -14,45 +12,12 @@ export const IndexPageTemplate = ({ image }) => (
   </div>
 );
 
-IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-};
-
-const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
-
+const IndexPage = () => {
   return (
     <Layout>
-      <IndexPageTemplate image={frontmatter.image} />
+      <IndexPageTemplate />
     </Layout>
   );
 };
 
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-};
-
 export default IndexPage;
-
-export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        description
-      }
-    }
-  }
-`;
