6d24dce2c06e9ff6d9b9eb9213d8d933c0223eb6
Dalton Saffe <daltonsaffe@gmail.com>
2020-05-04 21:23:10 -0400

    add idx pg to cms

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,16 +1,6 @@
 ---
 templateKey: index-page
-title: Champion Trees
-image: /img/home-jumbotron.jpg
-heading: Champion Tree Archive
-subheading: photographed & documented by Brian Kelley
-mainpitch:
-  title: Gathering Growth
-  description: National Champion Tree Program
-description: >-
-  United States Champion Tree Archive, Gathering Growth in partnership with
-  American Forests, owner of the National Champion Tree Program
-intro:
-  heading: Gathering Growth
-  description: National Champion Tree program
+title: Working to visually preserve the legacy of significant trees and forests in the United States.
 ---
+
+<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/409899424?color=9c833c" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,46 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Layout from '../components/Layout';
+import { graphql } from 'gatsby';
 import ContactCard from '../components/ContactCard.js';
+import Content, { HTMLContent } from '../components/Content';
 
-export const IndexPageTemplate = () => (
-  <div>
-    <section className="section mainContainer" id="idx">
-      <h1 className="idx-title">
-        Working to visually preserve the legacy of significant trees and forests
-        in the United States.
-      </h1>
-      <div
-        style={{
-          padding: `56.25% 0 0 0`,
-          position: `relative`,
-        }}
-      >
-        <iframe
-          src="https://player.vimeo.com/video/409899424"
-          style={{
-            position: `absolute`,
-            top: 0,
-            left: 0,
-            width: `100%`,
-            height: `100%`,
-          }}
-          title="Pine Tree Vid"
-          frameBorder="0"
-          allow="autoplay; fullscreen"
-          allowFullScreen
-        />
-      </div>
-      <ContactCard />
-    </section>
-  </div>
-);
+export const IndexPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content;
 
-const IndexPage = () => {
+  return (
+    <div>
+      <section className="section mainContainer" id="idx">
+        <h1 className="idx-title">{title}</h1>
+        <PageContent content={content} />
+        <ContactCard />
+      </section>
+    </div>
+  );
+};
+
+IndexPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+};
+
+const IndexPage = ({ data }) => {
+  const { markdownRemark: post } = data;
+
   return (
     <Layout>
-      <IndexPageTemplate />
+      <IndexPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content={post.html}
+      />
     </Layout>
   );
 };
 
+IndexPage.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
 export default IndexPage;
+
+export const IndexPageQuery = graphql`
+  query IndexPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
